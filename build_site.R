18360f6f6ee1b70e55335dfda18f85750dd94ee8
wanjiag <wanjiag@uoregon.edu>
2019-06-06 20:33:39 -0700

    website

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("C:/Users/Wanjia/OneDrive - University Of Oregon/Documents/Oregon/2019Spring/PSY607DataScience/git_practice_wanjia/git_hw_wanjia")
+
+#render your sweet site. 
+rmarkdown::render_site()
